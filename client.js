a101c0d1b539703aa394daf31f22dd201c9760c9
Jacob Bower <jacob.bower@student.manchester.ac.uk>
2019-06-11 12:46:59 +0100

    Basic UDP Client

---
+++ client.js
@@ -0,0 +1,20 @@
+const dgram = require("dgram");
+const vban = require("./vban");
+const server = dgram.createSocket("udp4");
+
+server.on("error", (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+
+server.on("message", (msg, rinfo) => {
+    console.log(vban.proccessPacket(msg));
+});
+
+server.on("listening", () => {
+    const address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(6980);
+// Prints: server listening 0.0.0.0:41234
